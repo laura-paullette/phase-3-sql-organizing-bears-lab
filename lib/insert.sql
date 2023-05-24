9b73cf39b3f8008ae3ec749217f4813ff919127d
laura-paullette <laura.paullette@student.moringaschool.com>
2023-05-24 20:57:41 +0300

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES  (1, 'Mr Chocolate', 12, 'M', 'Brown', 'Friendly', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES  (2, 'Rowdy', 3, 'M', 'Black', 'Cool', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES  (3, 'Tabitha', 10, 'F', 'White', 'Hostile', 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES  (4, 'Sergent Brown', 7, 'M', 'Brown', 'Friendly', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES  (5, 'Melissa', 14, 'F', 'Black', 'Cool', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES  (6, 'Grinch ', 9, 'M', 'Brown', 'Friendly', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES  (7, 'Wendy ', 3, 'F', 'White', 'Cool', 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES  (8, 'Null', 13, 'M', 'Black', 'Friendly', 0);
+            
